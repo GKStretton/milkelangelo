20ef931f99f28efa7195abc693c437b169903822
Greg Stretton <greg.stretton@protonmail.com>
2022-11-12 10:31:43 +0000

    move camera config into rtsp

--- services/rtsp/scripts/stream-top.sh
+++ services/rtsp/scripts/stream-top.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# must be called after camera is being read.
+/scripts/configure &
+
 gst-launch-1.0 v4l2src device=/dev/top-cam ! image/jpeg,width=1920,height=1080,framerate=30/1,format=MJPG ! rtspclientsink location=rtsp://localhost:8554/top-cam protocols=tcp
