16959a8ff925d483cd59b83802a9b93d2958ab9d
Greg Stretton <greg.stretton@protonmail.com>
2023-02-12 13:16:30 +0000

    Configure cameras

--- services/rtsp/scripts/configure-front.sh
+++ services/rtsp/scripts/configure-front.sh
@@ -9,19 +9,29 @@ v4l2-ctl --device $path -v width=1920,height=1080,pixelformat=MJPG --set-parm 60
 v4l2-ctl --device $path --get-fmt-video --get-parm
 
 v4l2-ctl --device $path --set-ctrl \
+brightness=128,\
+contrast=128,\
+saturation=170,\
+sharpness=128,\
+backlight_compensation=1\
+
+v4l2-ctl --device $path --set-ctrl \
 focus_auto=0,\
-exposure_auto=1
+exposure_auto=1,\
+white_balance_temperature_auto=0,\
+power_line_frequency=1
 
 # have to set focus twice because it doesn't engage if you set it to the
 # value as it already is. Seems to just stay at a silent 0 even though the value
 # changes
 v4l2-ctl --device $path --set-ctrl \
-focus_absolute=19
+focus_absolute=19,\
+white_balance_temperature=5000
 
 v4l2-ctl --device $path --set-ctrl \
 gain=1,\
 zoom_absolute=100,\
-exposure_absolute=200
+exposure_absolute=300
 
 
 # wait so focus set happens while camera is up (streamcam bug?)
--- services/rtsp/scripts/configure-top.sh
+++ services/rtsp/scripts/configure-top.sh
@@ -8,23 +8,33 @@ v4l2-ctl --device $path -v width=1920,height=1080,pixelformat=MJPG --set-parm 60
 v4l2-ctl --device $path --get-fmt-video --get-parm
 
 v4l2-ctl --device $path --set-ctrl \
+brightness=128,\
+contrast=128,\
+saturation=160,\
+sharpness=128,\
+backlight_compensation=0\
+
+v4l2-ctl --device $path --set-ctrl \
 focus_auto=0,\
-exposure_auto=1
+exposure_auto=1,\
+white_balance_temperature_auto=0,\
+power_line_frequency=1
 
 # have to set focus twice because it doesn't engage if you set it to the
 # value as it already is. Seems to just stay at a silent 0 even though the value
 # changes
 v4l2-ctl --device $path --set-ctrl \
-focus_absolute=27,\
+focus_absolute=29,\
+white_balance_temperature=5000
 
 v4l2-ctl --device $path --set-ctrl \
 gain=1,\
 zoom_absolute=120,\
-exposure_absolute=60
+exposure_absolute=70
 
 # wait so focus set happens while camera is up (streamcam bug?)
 sleep 1.5
-v4l2-ctl --device $path --set-ctrl focus_absolute=28
+v4l2-ctl --device $path --set-ctrl focus_absolute=30
 
 # Fix for red camera effect
 v4l2-ctl --device $path --set-ctrl gain=255
