7b9fb408138812d9fb6ec8916b73ef042a9b27e4
Greg Stretton <greg.stretton@protonmail.com>
2023-02-24 21:11:52 +0000

    fix yet another race

--- services/goo/livecapture/capture.go
+++ services/goo/livecapture/capture.go
@@ -20,21 +20,21 @@ func (r *recorder) record(id session.ID) {
 	// webcam recordings
 
 	var topRecording, frontRecording *webcamRecorder
-	var err error
+	var errTop, errFront error
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		topRecording, err = startWebcamRecording(*config.TopCamRtspPath, uint64(id))
-		if err != nil {
-			fmt.Printf("failed to start top webcam recording: %v\n", err)
+		topRecording, errTop = startWebcamRecording(*config.TopCamRtspPath, uint64(id))
+		if errTop != nil {
+			fmt.Printf("failed to start top webcam recording: %v\n", errTop)
 		}
 	}()
 	go func() {
 		defer wg.Done()
-		frontRecording, err = startWebcamRecording(*config.FrontCamRtspPath, uint64(id))
-		if err != nil {
-			fmt.Printf("failed to start front webcam recording: %v\n", err)
+		frontRecording, errFront = startWebcamRecording(*config.FrontCamRtspPath, uint64(id))
+		if errFront != nil {
+			fmt.Printf("failed to start front webcam recording: %v\n", errFront)
 		}
 	}()
 	wg.Wait()
