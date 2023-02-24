98cd5757e0e137255d0a88d448f517b64fe1f13e
Greg Stretton <greg.stretton@protonmail.com>
2023-02-24 21:28:53 +0000

    what a surprise, it's 60

--- services/rtsp/scripts/stream-top.sh
+++ services/rtsp/scripts/stream-top.sh
@@ -3,4 +3,4 @@
 # must be called after camera is being read.
 /scripts/configure-top.sh &
 
-gst-launch-1.0 v4l2src device=/dev/top-cam ! image/jpeg,width=1920,height=1080,framerate=30/1,format=MJPG ! rtspclientsink location=rtsp://localhost:8554/top-cam protocols=tcp
+gst-launch-1.0 v4l2src device=/dev/top-cam ! image/jpeg,width=1920,height=1080,framerate=60/1,format=MJPG ! rtspclientsink location=rtsp://localhost:8554/top-cam protocols=tcp
