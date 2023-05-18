e7b24447bc269447ba714561fc120a65475c7071
Greg Stretton <greg.stretton@protonmail.com>
2023-05-18 09:20:27 +0100

    add stills posts

--- user-tools/content_plan/still.py
+++ user-tools/content_plan/still.py
@@ -1,4 +1,30 @@
 from machinepb import machine as pb
 
 def buildStill(n_str: str) -> pb.ContentTypeStatus:
-	return None
+	ct = pb.ContentType.CONTENT_TYPE_STILL
+
+	s = pb.ContentTypeStatus(
+		raw_title=f"Piece {n_str} - A Study of Light",
+		raw_description="",
+		caption="define captions!",
+	)
+
+	platform = pb.SocialPlatform.SOCIAL_PLATFORM_YOUTUBE
+	s.posts.append(pb.Post(
+		platform=platform,
+		title=f"{s.raw_title}\n\n{get_common_text(ct, platform)}\n\n{get_hashtags(ct, platform)}",
+		description="N/A",
+		crosspost=False,
+		scheduled_unix_timetamp=get_schedule_timestamp(ct),
+	))
+
+	platform = pb.SocialPlatform.SOCIAL_PLATFORM_INSTAGRAM
+	s.posts.append(pb.Post(
+		platform=platform,
+		title=f"{s.raw_title}\n\n{get_common_text(ct, platform)}\n\n{get_hashtags(ct, platform)}",
+		description="N/A",
+		crosspost=False,
+		scheduled_unix_timetamp=get_schedule_timestamp(ct),
+	))
+
+	return s
