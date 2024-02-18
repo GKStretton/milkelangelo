b6d4fe5dfdeebddaba7b933995ab419ec2aee7ea
Greg Stretton <greg.stretton@protonmail.com>
2024-02-18 21:46:09 +0000

    update z

--- firmware/src/extras/topics_firmware/topics_firmware.h
+++ firmware/src/extras/topics_firmware/topics_firmware.h
@@ -1,7 +1,7 @@
 #ifndef MQTT_CONSTANTS_H
 #define MQTT_CONSTANTS_H
 
-#define IK_Z_LEVEL_MM "42"
+#define IK_Z_LEVEL_MM "43"
 #define TOPIC_COLLECT "mega/req/collect"
 #define TOPIC_DEBUG_DATA "mega/req/debug-data"
 #define TOPIC_DISPENSE "mega/req/dispense"
