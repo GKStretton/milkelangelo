c490c17cd3e0c5b9a7655f61fad5b12bd029ff1e
Greg Stretton <greg.stretton@protonmail.com>
2024-06-08 19:36:37 +0100

    fix subscription

--- goo/events/helpers.go
+++ goo/events/helpers.go
@@ -3,11 +3,11 @@ package events
 import "github.com/gkstretton/asol-protos/go/machinepb"
 
 func ConditionWaiter(cond func(*machinepb.StateReport) bool) chan *machinepb.StateReport {
-	c := Subscribe()
-	defer Unsubscribe(c)
-
 	filterChan := make(chan *machinepb.StateReport)
 	go func() {
+		c := Subscribe()
+		defer Unsubscribe(c)
+
 		for {
 			r := <-c
 			if cond(r) {
