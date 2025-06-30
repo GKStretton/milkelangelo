bf96daa6db5f0665f8f8f1a1e35c6626bf32a2a3
Greg Stretton <greg.stretton@protonmail.com>
2025-06-30 21:08:23 +0100

    remove config

--- twitch-extension/frontend/source/context/twitch.tsx
+++ twitch-extension/frontend/source/context/twitch.tsx
@@ -2,24 +2,8 @@ import React from "react";
 
 import { setAuthorization } from "~api/twitch";
 
-interface Configuration {
-	global: {
-		version: string;
-		content: string;
-	};
-	developer: {
-		version: string;
-		content: string;
-	};
-	broadcaster: {
-		version: string;
-		content: string;
-	};
-}
-
 interface TwitchContext {
 	authorized: Twitch.ext.Authorized | null;
-	configuration: Configuration | null;
 }
 
 const twitchContext = React.createContext<TwitchContext>(null as any);
@@ -28,27 +12,15 @@ const TwitchProvider = (props: React.PropsWithChildren) => {
 	const [authorized, setAuthorized] =
 		React.useState<Twitch.ext.Authorized | null>(null);
 
-	const [configuration, setConfiguration] =
-		React.useState<Configuration | null>(null);
-
 	React.useEffect(() => {
 		Twitch.ext.onAuthorized((authorized) => {
 			setAuthorization(authorized.helixToken, authorized.clientId);
 			setAuthorized(authorized);
 		});
-
-		Twitch.ext.configuration.onChanged(() => {
-			setConfiguration({
-				global: Twitch.ext.configuration.global,
-				developer: Twitch.ext.configuration.developer,
-				broadcaster: Twitch.ext.configuration.broadcaster,
-			});
-		});
 	}, []);
 
 	const context: TwitchContext = {
 		authorized,
-		configuration,
 	};
 
 	if (!authorized) {
